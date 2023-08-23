5936592cd11ef2f1b93d41a0e206acfd89dbf625
evgeny azeniz <azenizzka@gmail.com>
2023-08-24 02:00:52 +0700

    Added mods: 1. Auto && 2. timeToJoin

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,28 @@ import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.Timer;
 
 public class Main {
     private static final boolean[] found = new boolean[1];
     private static boolean isActive = false;
 
+    private static int mode = 1; // 0 - auto    1 - time
+
+    private static int minutesToJoin = 58; // minutes, while game must be started
+    private static int minutesToExit = 2; // minutes, while game must be shutdown
+
+    private static String process = "gta_sa.exe";
+
+    private static String nickname = "Sam_Mason";
+    private static String serverIp = "80.66.82.168";
+    private static String serverPort = "7777";
+
     public static void main(String[] args) throws IOException {
         isActive = true;
-        begin("gta_sa.exe");
+        begin(process);
     }
 
     public static void resetValues() {
@@ -21,37 +34,46 @@ public class Main {
 
     public static void begin(String processToSearch) throws IOException {
         while (isActive){
-            Process process = new ProcessBuilder("tasklist.exe", "/fo", "csv", "/nh").start();
-            Scanner sc = new Scanner(process.getInputStream());
-            if (sc.hasNextLine()) sc.nextLine();
-            while (sc.hasNextLine()) {
-                String line = sc.nextLine();
-                String[] parts = line.split(",");
-                String processName = parts[0].substring(1).replaceFirst(".$", "");
-                if(processName.equalsIgnoreCase(processToSearch)) {
-                    found[0] = true;
+            if(mode == 0) {
+                Process process = new ProcessBuilder("tasklist.exe", "/fo", "csv", "/nh").start();
+                Scanner sc = new Scanner(process.getInputStream());
+                if (sc.hasNextLine()) sc.nextLine();
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    String[] parts = line.split(",");
+                    String processName = parts[0].substring(1).replaceFirst(".$", "");
+                    if(processName.equalsIgnoreCase(processToSearch)) {
+                        found[0] = true;
+                    }
                 }
-            }
 
-            try {
-                process.waitFor();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-
-            if(found[0]) {
-                System.out.println(Colors.ANSI_GREEN + "Был найден процесс " + Colors.ANSI_CYAN + processToSearch + Colors.ANSI_RESET);
                 try {
-                    checkCrash();
-                } catch (IOException e) {
+                    process.waitFor();
+                } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-            } else {
-                System.out.println(Colors.ANSI_RED + "Процесс не был найден " + Colors.ANSI_CYAN + processToSearch + Colors.ANSI_RESET);
-                Launch.samp("Jamal_Blackberry", "80.66.82.168", "7777");
+
+
+                if(found[0]) {
+                    System.out.println(Colors.ANSI_GREEN + "Был найден процесс " + Colors.ANSI_CYAN + processToSearch + Colors.ANSI_RESET);
+                    try {
+                        checkCrash();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    System.out.println(Colors.ANSI_RED + "Процесс не был найден " + Colors.ANSI_CYAN + processToSearch + Colors.ANSI_RESET);
+                    Launch.samp(nickname, serverIp, serverPort);
+                }
+                resetValues();
+            } else if(mode == 1) {
+                Date date = new Date();
+                if(date.getMinutes() == minutesToJoin) {
+                    Launch.samp(nickname, serverIp, serverPort);
+                } else if(date.getMinutes() == minutesToExit) {
+                    killProcess(process);
+                }
             }
-            resetValues();
         }
     }
 
@@ -62,7 +84,7 @@ public class Main {
             System.out.println(Colors.ANSI_RED + "CLIPBOARD: " + Colors.ANSI_WHITE + '\"' + Colors.ANSI_CYAN + data + Colors.ANSI_WHITE + '\"' + Colors.ANSI_RESET);
             if(data.startsWith("https://crash.sr.team/?uid=")) {
                 clearClipBoard();
-                killProcess("gta_sa.exe");
+                killProcess(process);
             }
         } catch (UnsupportedFlavorException e) {
             System.out.println(e.getMessage());
@@ -73,12 +95,6 @@ public class Main {
         StringSelection stringSelection = new StringSelection("RELOAD SAMP");
         Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
     }
-
-    static int i;
-    static{
-        i = 47;
-    }
-
 
     public static void killProcess(String process) throws IOException {
         Process exec = Runtime.getRuntime().exec("taskkill /F /T /im " + process);
